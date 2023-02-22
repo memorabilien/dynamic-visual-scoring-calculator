4c6e12ffcfd6b02e27143dbd6c8ed81d2aceae4b
memorabilien <management@memorabilien.org>
2023-02-22 08:34:28 +0100

    clean up

--- src/calcWeights.js
+++ src/calcWeights.js
@@ -19,180 +19,143 @@ DEALINGS IN THE SOFTWARE.
 
 /**
  * calculates the other weight percentages, which are not the input weight
- * @param {Array} inputMatrix 
- * @param {Array} inputVektor 
+ * @param {Array} inputMatrix
+ * @param {Array} inputVektor
  * @returns {Array} other weights in percent
  */
-export default function calcWeight(inputMatrix, inputVector){
-
-  let output;
-      /*
+export default function calcWeight(inputMatrix, inputVector) {
+	let output;
+	/*
         Made with matrix.js from Raghavendra Ravikumar
         MIT License copyright (c) 2016 Raghavendra Ravikumar
         https://github.com/RaghavCodeHub/matrix/blob/master/LICENSE-MIT
       */
-		function rational(t, n) {
-			return (
-				(n = n || 1),
-				-1 === Math.sign(n) && ((t = -t), (n = -n)),
-				{ num: t, den: n, add: (e) => rational(t * e.den + n * e.num, n * e.den), sub: (e) => rational(t * e.den - n * e.num, n * e.den), mul: (e) => multiply(e, t, n), div: (e) => multiply(rational(e.den, e.num), t, n) }
-			);
-		}
-		function multiply(t, n, e) {
-
-			let r = Math.sign(n) * Math.sign(t.num),
-				i = Math.sign(e) * Math.sign(t.den);
-			return (
-				Math.abs(n) === Math.abs(t.den) && Math.abs(e) === Math.abs(t.num)
-					? ((r = r), (i = i))
-					: Math.abs(e) === Math.abs(t.num)
-					? ((r *= Math.abs(n)), (i *= Math.abs(t.den)))
-					: Math.abs(n) === Math.abs(t.den)
-					? ((r *= Math.abs(t.num)), (i *= Math.abs(e)))
-					: ((r = n * t.num), (i = e * t.den)),
-				rational(r, i)
-			);
-		}
-		function merge(t) {
-
-
-			return { top: (n) => top(t, n), bottom: (n) => bottom(t, n), left: (n) => left(t, n), right: (n) => right(t, n) };
-		}
-		function matrix(t) {
-
-
-			if (!Array.isArray(t)) throw new Error("Input should be of type array");
-			return Object.assign(function () {
-				let n = 1 === arguments.length ? [arguments[0]] : Array.apply(null, arguments);
-				return read(t, n);
-			}, _mat(t));
-		}
-		function _mat(t) {
-
-			return {
-				size: () => size(t),
-				add: (n) => operate(t, n, addition),
-				sub: (n) => operate(t, n, subtraction),
-				mul: (n) => operate(t, n, multiplication),
-				div: (n) => operate(t, n, division),
-				prod: (n) => prod(t, n),
-				trans: () => trans(t),
-				set: function () {
-					let n = 1 === arguments.length ? [arguments[0]] : Array.apply(null, arguments);
-					return { to: (e) => replace(t, e, n) };
-				},
-				det: () => determinant(t),
-				inv: () => invert(t),
-				merge: merge(t),
-				map: (n) => map(t, n),
-				equals: (n) => equals(t, n),
-			};
-		}
-		function size(t) {
-			let n = [];
-			for (; Array.isArray(t); ) n.push(t.length), (t = t[0]);
-			return n;
-		}
-		function read(t, n) {
-			return 0 === n.length ? t : extract(t, n);
-		}
-		function prod(t, n) {
-
-			
-			let e = t,
-				r = n(),
-				i = size(e),
-				a = size(r),
-				l = [];
-			if (i[1] === a[0])
-				for (let t = 0; t < i[0]; t++) {
-					l[t] = [];
-					for (let n = 0; n < a[1]; n++) for (let a = 0; a < i[1]; a++) void 0 === l[t][n] && (l[t][n] = 0), (l[t][n] += multiplication(e[t][a], r[a][n]));
-				}
-			return l;
-		}
-		function multiplication(t, n) {
-			return t * n;
-		}
-		function invert(t) {
-
-			let n = rationalize(t),
-				e = size(t),
-				r = rationalize(identity(e[0])),
-				i = 0,
-				a = 0;
-			for (; a < e[0]; ) {
-				if (0 === n[i][a].num) for (let t = i + 1; t < e[0]; t++) 0 !== n[t][a].num && (interchange(n, i, t), interchange(r, i, t));
-				if (0 !== n[i][a].num) {
-					if (1 !== n[i][a].num || 1 !== n[i][a].den) {
-						let t = rational(n[i][a].num, n[i][a].den);
-						for (let a = 0; a < e[1]; a++) (n[i][a] = n[i][a].div(t)), (r[i][a] = r[i][a].div(t));
-					}
-					for (let t = i + 1; t < e[0]; t++) {
-						let l = n[t][a];
-						for (let a = 0; a < e[1]; a++) (n[t][a] = n[t][a].sub(l.mul(n[i][a]))), (r[t][a] = r[t][a].sub(l.mul(r[i][a])));
-					}
-				}
-				(i += 1), (a += 1);
+	function rational(t, n) {
+		return (
+			(n = n || 1),
+			-1 === Math.sign(n) && ((t = -t), (n = -n)),
+			{ num: t, den: n, add: (e) => rational(t * e.den + n * e.num, n * e.den), sub: (e) => rational(t * e.den - n * e.num, n * e.den), mul: (e) => multiply(e, t, n), div: (e) => multiply(rational(e.den, e.num), t, n) }
+		);
+	}
+	function multiply(t, n, e) {
+		let r = Math.sign(n) * Math.sign(t.num),
+			i = Math.sign(e) * Math.sign(t.den);
+		return (
+			Math.abs(n) === Math.abs(t.den) && Math.abs(e) === Math.abs(t.num)
+				? ((r = r), (i = i))
+				: Math.abs(e) === Math.abs(t.num)
+				? ((r *= Math.abs(n)), (i *= Math.abs(t.den)))
+				: Math.abs(n) === Math.abs(t.den)
+				? ((r *= Math.abs(t.num)), (i *= Math.abs(e)))
+				: ((r = n * t.num), (i = e * t.den)),
+			rational(r, i)
+		);
+	}
+	function matrix(t) {
+		if (!Array.isArray(t)) throw new Error("Input should be of type array");
+		return Object.assign(function () {
+			let n = 1 === arguments.length ? [arguments[0]] : Array.apply(null, arguments);
+			if(0 === n.length ){
+				return t;
 			}
-			let l = e[0] - 1;
-			if (1 !== n[l][l].num || 1 !== n[l][l].den) {
-				let t = rational(n[l][l].num, n[l][l].den);
-				for (let i = 0; i < e[1]; i++) (n[l][i] = n[l][i].div(t)), (r[l][i] = r[l][i].div(t));
+		}, _mat(t));
+	}
+	function _mat(t) {
+		return {
+			size: () => size(t),
+			prod: (n) => prod(t, n),
+			inv: () => invert(t),
+		};
+	}
+	function size(t) {
+		let n = [];
+		for (; Array.isArray(t); ) n.push(t.length), (t = t[0]);
+		return n;
+	}
+	function prod(t, n) {
+		let e = t,
+			r = n(),
+			i = size(e),
+			a = size(r),
+			l = [];
+		if (i[1] === a[0])
+			for (let t = 0; t < i[0]; t++) {
+				l[t] = [];
+				for (let n = 0; n < a[1]; n++) for (let a = 0; a < i[1]; a++) void 0 === l[t][n] && (l[t][n] = 0), (l[t][n] += (e[t][a]* r[a][n]));
 			}
-			for (let t = e[0] - 1; t > 0; t--)
-				for (let i = t - 1; i >= 0; i--) {
-					let a = rational(-n[i][t].num, n[i][t].den);
-					for (let l = 0; l < e[1]; l++) (n[i][l] = a.mul(n[t][l]).add(n[i][l])), (r[i][l] = a.mul(r[t][l]).add(r[i][l]));
+		return l;
+	}
+	function invert(t) {
+		let n = rationalize(t),
+			e = size(t),
+			r = rationalize(identity(e[0])),
+			i = 0,
+			a = 0;
+		for (; a < e[0]; ) {
+			if (0 === n[i][a].num) for (let t = i + 1; t < e[0]; t++) 0 !== n[t][a].num && (interchange(n, i, t), interchange(r, i, t));
+			if (0 !== n[i][a].num) {
+				if (1 !== n[i][a].num || 1 !== n[i][a].den) {
+					let t = rational(n[i][a].num, n[i][a].den);
+					for (let a = 0; a < e[1]; a++) (n[i][a] = n[i][a].div(t)), (r[i][a] = r[i][a].div(t));
 				}
-			return derationalize(r);
-		}
-		function rationalize(t) {
-
-			let n = [];
-			return (
-				t.forEach((t, e) => {
-					n.push(t.map((t) => rational(t)));
-				}),
-				n
-			);
-		}
-		function derationalize(t) {
-
-
-			let n = [];
-			return (
-				t.forEach((t, e) => {
-					n.push(t.map((t) => t.num / t.den));
-				}),
-				n
-			);
-		}
-		function generate(t, n) {
-
-
-			let e = 2;
-			for (; e > 0; ) {
-				for (var r = [], i = 0; i < t; i++) Array.isArray(n) ? r.push(Object.assign([], n)) : r.push(n);
-				(n = r), (e -= 1);
+				for (let t = i + 1; t < e[0]; t++) {
+					let l = n[t][a];
+					for (let a = 0; a < e[1]; a++) (n[t][a] = n[t][a].sub(l.mul(n[i][a]))), (r[t][a] = r[t][a].sub(l.mul(r[i][a])));
+				}
 			}
-			return n;
+			(i += 1), (a += 1);
 		}
-		function identity(t) {
-
-			let n = generate(t, 0);
-			return (
-				n.forEach((t, n) => {
-					t[n] = 1;
-				}),
-				n
-			);
+		let l = e[0] - 1;
+		if (1 !== n[l][l].num || 1 !== n[l][l].den) {
+			let t = rational(n[l][l].num, n[l][l].den);
+			for (let i = 0; i < e[1]; i++) (n[l][i] = n[l][i].div(t)), (r[l][i] = r[l][i].div(t));
 		}
+		for (let t = e[0] - 1; t > 0; t--)
+			for (let i = t - 1; i >= 0; i--) {
+				let a = rational(-n[i][t].num, n[i][t].den);
+				for (let l = 0; l < e[1]; l++) (n[i][l] = a.mul(n[t][l]).add(n[i][l])), (r[i][l] = a.mul(r[t][l]).add(r[i][l]));
+			}
+		return derationalize(r);
+	}
+	function rationalize(t) {
+		let n = [];
+		return (
+			t.forEach((t, e) => {
+				n.push(t.map((t) => rational(t)));
+			}),
+			n
+		);
+	}
+	function derationalize(t) {
+		let n = [];
+		return (
+			t.forEach((t, e) => {
+				n.push(t.map((t) => t.num / t.den));
+			}),
+			n
+		);
+	}
+	function generate(t, n) {
+		let e = 2;
+		for (; e > 0; ) {
+			for (var r = [], i = 0; i < t; i++) Array.isArray(n) ? r.push(Object.assign([], n)) : r.push(n);
+			(n = r), (e -= 1);
+		}
+		return n;
+	}
+	function identity(t) {
+		let n = generate(t, 0);
+		return (
+			n.forEach((t, n) => {
+				t[n] = 1;
+			}),
+			n
+		);
+	}
 
-		let A = matrix(inputMatrix);
-		let b = matrix(inputVector);
-		let MatrixInverse = A.inv();
-		output = prod(MatrixInverse, b);
-		return output.flat();
+	let A = matrix(inputMatrix);
+	let b = matrix(inputVector);
+	let MatrixInverse = A.inv();
+	output = prod(MatrixInverse, b);
+	return output.flat();
 }
-
